489e75dae5fbc7f2b87ecbf150e5d23f239eb6d3
Justin Chang <justin.chang@goldenhippo.com>
2019-02-26 15:15:07 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="./reset.css" rel="stylesheet">
+    <link href="./style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Enriqueta:400,700" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="page">
+        <header>
+            <div class="container">
+                <p class="header_phone">(000) 000-0000</p>
+                <img class="logo" src="./images/logo@3x.png">
+            </div>
+        </header>
+        <div class="container sound">
+            <h1 class="sound_header">
+                <img class="sound_icon" src="./images/sound icon@3x.png">
+                <span class="sound_text">MAKE SURE YOUR SOUND IS ON</span>
+                <img class="sound_icon" src="./images/sound icon@3x.png">
+            </h1>
+        </div>
+        <div class="video">
+            
+            </div>
+        <div class="container">
+            <button class="container next_step">NEXT STEP</button>
+        </div>
+        <div class="container melina">
+            <img class="melina_img" src="./images/DrR photo1x.png">
+            <div class="melina_title">
+                <h1 class="melina_heading">DR. MELINA</h1>
+                <h2 class="nutritionalist">Nutritionalist</h2>
+            </div>
+            <div class="melina_bio">
+                <p>Eum in adhuc mollis, eos id ignota eirmod argumentum. Etiam antiopam splendide quo ea, error decore concludaturque est no, justo putant ne has. Usu ne percipit deterruisset, in omnis oportere pri, nam an vidisse propriae inimicus. Eum in adhuc mollis, eos id ignota eirmod argumentum.</p>
+            </div>
+        </div>
+    </div>
+    <div class="info">
+        <div class="info_header">
+            <h1>IN THIS PRESENTATION YOU'RE GOING TO LEARN:</h1>
+        </div>
+        <div class="info_info center">
+            <ul>
+                <li>3-step Energy & Digestive Health Protocol.</li>
+                <li>How many times a day you should actually be "eliminating".</li>
+                <li>How to avoid caffeine-crashes</li>
+                <li>What the digestive system craves first thing in the morning.</li>
+            </ul>
+        </div>
+    </div>
+    
+</body>
+</html>
