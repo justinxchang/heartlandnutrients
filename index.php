80d3d8612d3ee33678c8b63b52bebd2aae021a7c
Justin Chang <justin.chang@goldenhippo.com>
2019-02-26 17:56:35 -0800

    done with most of layout

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                 <img class="logo" src="./images/logo@3x.png">
             </div>
         </header>
-        <div class="container sound">
+        <div class="container">
             <h1 class="sound_header">
                 <img class="sound_icon" src="./images/sound icon@3x.png">
                 <span class="sound_text">MAKE SURE YOUR SOUND IS ON</span>
@@ -46,15 +46,45 @@
         <div class="info_header">
             <h1>IN THIS PRESENTATION YOU'RE GOING TO LEARN:</h1>
         </div>
-        <div class="info_info center">
+        <div class="info_info container">
             <ul>
                 <li>3-step Energy & Digestive Health Protocol.</li>
-                <li>How many times a day you should actually be "eliminating".</li>
                 <li>How to avoid caffeine-crashes</li>
+                <li>How many times a day you should actually be "eliminating".</li>
                 <li>What the digestive system craves first thing in the morning.</li>
             </ul>
         </div>
     </div>
+    <div class="citations container">
+        <h2 class="citations_header">CITATIONS:</h1>
+        <div class="citations_text">
+            <p>Salvati E. Tubular duplication of the colon:. Disease of the Colon and Rectum. 1971;14(3):206-212.</p>
+            <p>Yeldham C. Living History: Cooking and Fire. Petits Propos Culinaires. 2007;84:121-126.</p>
+            <p>Cordain L, et. al. Origins and evolution of the Western diet: health implications for the 21st century. American Journal of Clinical Nutrition. 2005;81(2):341-354.</p>
+            <p>Logan F. A History Of The Church In The Middle Ages. London: Routledge; 2013.</p>
+            <p>Naum Jasny , "The Daily Bread of the Ancient Greeks and Romans," Osiris 9 (1950): 227-253.</p>
+            <p>Mayyasi A. Why Cereal Has Such Aggressive Marketing. The Atlantic. 2016.</p>
+            <p>Smith A. Food And Drink In American History: A "Full Course" Encyclopedia. Denver, CO: ABC-CLIO; 2013.</p>
+            <p>Colleary, E. How Bacon and Eggs Became the American Breakfast. The American Table. 2012. Available at: http://www.americantable.org/2012/07/how-bacon-and-eggs-became-the-american-breakfast/. Accessed December 13, 2017.</p>
+            <p>Dunn R. The Hidden Truths about Calories. Scientific American. 2012.</p>
+            <p>Bakalar N. A High-Fat Diet May Lead to Daytime Sleepiness. The New York Times. 2016. Available at: https://well.blogs.nytimes.com/2016/04/21/a-high-fat-diet-may-lead-to-daytime-sleepiness/?_r=0. Accessed December 14, 2017.</p>
+            <p>Ensure® Original Milk Chocolate Nutrition Shake | Complete Nutrition Info. Ensurecom. Available at: https://ensure.com/nutrition-products/ensure-original. Accessed December 14, 2017</p>
+            <p>Snickers®. Snickerscom. 2017. Available at: https://www.snickers.com/Nutritional-Info#SnickersFunSize. Accessed December 14, 2017.</p>
+            <p>Ganguly R, Pierce G. Trans fat involvement in cardiovascular disease. Molecular Nutrition & Food Research. 2012;56(7):1090-1096. doi:10.1002/mnfr.201100700.</p>
+            <p>Azadbakht L, Kimiagar M, Mehrabi Y, Esmaillzadeh A, Hu F, Willett W. Soy Consumption, Markers of Inflammation, and Endothelial Function: A cross-over study in postmenopausal women with the metabolic syndrome. Diabetes Care. 2007;30(4):967-973. doi:10.2337/dc06-2126.</p>
+            <p>Melnik B. Evidence for Acne-Promoting Effects of Milk and Other Insulinotropic Dairy Products. Milk and Milk Products in Human Nutrition. 2011:131-145. doi:10.1159/000325580.</p>
+            <p> .</p>
+        </div>
+    </div>
+    <footer class="container">
+        <h2 class="footer_title container">© 2018 Heartland Nutrients</h2>
+        <div class="footer_text">
+            <p>
+                These statements haven't been evaluated by the Food & Drug Administration. These products aren't intended to help diagnose, treat, cure, or prevent any disease. Use only as directed. Consult your healthcare provider before using supplements. The information provided herein is intended for your general knowledge only and is not intended to be, nor is it, medical advice or a substitute for medical advice, nor is this information intended to be, nor is it, treatment for specific medical conditions. You should not use this information to treat, diagnose, or attempt to cure a health problem or disease. If you have, or suspect you have, a specific medical condition or disease, please consult your healthcare provider. Please consult your pediatrician before you give this product to a child under 18 years of age.
+            </p>
+        </div>
+        <h2 class="footer_privacy container">Privacy | Terms & Conditions</h2>
+    </footer>
     
 </body>
 </html>
